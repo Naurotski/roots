f0f0731629dd10316962bfd130f5d6e1fe2d5d8d
Naurotski <6602796@gmail.com>
2023-04-19 21:42:38 +0200

    HomePage.vue - carousel

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,14 +1,38 @@
 <template>
   <transition appear enter-active-class="animated fadeIn" leave-active-class="animated fadeOut">
     <q-page>
-      <q-img alt="logo" src="~assets/3F0A9210.jpg" class="fixed" height="90vh" />
+      <!--      <q-img alt="logo" src="~assets/3F0A9210.jpg" class="fixed" height="90vh" />-->
+      <q-carousel
+        class="fixed"
+        animated
+        v-model="slide"
+        height="85vh"
+        style="width: 100%"
+        autoplay
+        swipeable
+        infinite
+        transition-duration="3000"
+      >
+        <q-carousel-slide
+          v-for="({ urlImageWork, artistName }, index) in allWorks"
+          :name="index"
+          :img-src="urlImageWork"
+          :key="urlImageWork"
+        >
+          <div class="absolute-bottom custom-caption">
+            <div class="text-h2">{{ artistName }}</div>
+          </div>
+        </q-carousel-slide>
+      </q-carousel>
     </q-page>
   </transition>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import { useMeta } from 'quasar'
+import { useArtistsStore } from 'stores/artists-store.js'
+import { storeToRefs } from 'pinia'
 
 const metaData = {
   title: 'Roots Gallery',
@@ -26,7 +50,23 @@ const metaData = {
 export default defineComponent({
   name: 'HomePage',
   setup() {
+    const artistsStore = useArtistsStore()
+    const { filterArtistsDraft, allWorks } = storeToRefs(artistsStore)
+    const { getArtists } = artistsStore
+    if (!filterArtistsDraft.value.length) getArtists()
+    const slide = ref(0)
     useMeta(metaData)
+    return {
+      allWorks,
+      slide
+    }
   }
 })
 </script>
+<style lang="sass" scoped>
+.custom-caption
+  text-align: center
+  padding: 80px
+  color: white
+  background-color: rgba(0, 0, 0, .3)
+</style>
