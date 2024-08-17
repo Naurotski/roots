19f715c572f3907b3911eab9c4a10b8407e8e701
Naurotski <6602796@gmail.com>
2024-08-17 18:09:34 +0200

    <q-img :src="urlWorkShowcase || urlPortrait"

--- src/pages/ArtistsListPage.vue
+++ src/pages/ArtistsListPage.vue
@@ -4,12 +4,18 @@
       <fixed-top-title :name="$t('links.artists')" />
       <div class="q-gutter-md row wrap justify-around q-my-md">
         <q-card
-          v-for="{ artistId, firstName, lastName, urlPortrait } in filterArtistsDraft"
+          v-for="{
+            artistId,
+            firstName,
+            lastName,
+            urlPortrait,
+            urlWorkShowcase
+          } in filterArtistsDraft"
           :key="artistId"
         >
           <router-link :to="`/artists/${artistId}`">
             <!--удалить размер при правильной фотографии-->
-            <q-img :src="urlPortrait" style="height: 300px; width: 300px">
+            <q-img :src="urlWorkShowcase || urlPortrait" style="height: 300px; width: 300px">
               <div class="absolute-bottom text-body1 text-center">
                 {{ firstName }} {{ lastName }}
               </div></q-img
