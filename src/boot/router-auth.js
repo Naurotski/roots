5c714779b92a24724c561fcce47a19f55930522b
Naurotski <6602796@gmail.com>
2025-09-02 19:03:41 +0200

    router-auth.js if (to.name === '3D Gallery')

--- src/boot/router-auth.js
+++ src/boot/router-auth.js
@@ -1,12 +1,24 @@
 import { boot } from 'quasar/wrappers'
 import { LocalStorage } from 'quasar'
+import { useGraphics3DStore } from 'stores/graphics3D-store'
+import { storeToRefs } from 'pinia'
 
 export default boot(({ router }) => {
   router.beforeEach((to) => {
     if (
       !LocalStorage.getItem('loggedIn') &&
       (to.name === 'Your Account' || to.name === '3D Gallery')
-    )
+    ) {
       return { name: 'Home' }
+    }
+    if (to.name === '3D Gallery') {
+      const graphicsStore = useGraphics3DStore()
+      const id = String(to.params.galleryId)
+      const { filteredListGalleriesNonDraft } = storeToRefs(graphicsStore)
+      const payment = filteredListGalleriesNonDraft.value[id]?.payment
+      if (!payment) {
+        return { name: 'Home' } // или на страницу оплаты/превью
+      }
+    }
   })
 })
