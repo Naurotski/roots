cf386f9dbc32db9806b7dda8aad57cefa0b236a7
Naurotski <6602796@gmail.com>
2025-09-15 11:56:13 +0200

    router-auth.js

--- src/boot/router-auth.js
+++ src/boot/router-auth.js
@@ -1,10 +1,12 @@
 import { boot } from 'quasar/wrappers'
-import { LocalStorage } from 'quasar'
-import { useGraphics3DStore } from 'stores/graphics3D-store'
 import { storeToRefs } from 'pinia'
+import { LocalStorage, Loading } from 'quasar'
+import { useGraphics3DStore } from 'stores/graphics3D-store'
+
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms))
 
 export default boot(({ router }) => {
-  router.beforeEach((to) => {
+  router.beforeEach(async (to) => {
     if (
       !LocalStorage.getItem('loggedIn') &&
       (to.name === 'Your Account' || to.name === '3D Gallery')
@@ -13,11 +15,23 @@ export default boot(({ router }) => {
     }
     if (to.name === '3D Gallery') {
       const graphicsStore = useGraphics3DStore()
-      const id = String(to.params.galleryId)
       const { filteredListGalleriesNonDraft } = storeToRefs(graphicsStore)
-      const payment = filteredListGalleriesNonDraft.value[id]?.payment
+      const { listenForChildEvents } = graphicsStore
+      const id = String(to.params.galleryId)
+      let payment = filteredListGalleriesNonDraft.value[id]?.payment
       if (!payment) {
-        return { name: 'Home' } // или на страницу оплаты/превью
+        // запускаем загрузку и ждём немного
+        listenForChildEvents('listGalleries')
+        Loading.show()
+        try {
+          await sleep(2000)
+          payment = filteredListGalleriesNonDraft.value[id]?.payment
+          if (!payment) {
+            return { name: 'Home' }
+          }
+        } finally {
+          Loading.hide()
+        }
       }
     }
   })
